d425ad9e4c7247f49f2c665dee55aeb9091c48ba
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-12-03 19:21:45 +0100

    simplified consumer

--- consumer/src/main/java/com/example/demo/DemoConsumerApplication.java
+++ consumer/src/main/java/com/example/demo/DemoConsumerApplication.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -18,27 +14,11 @@ public class DemoConsumerApplication {
 
 	final static String queueName = "sample-queue";
 
-	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
-	}
-
 
 	@Bean
 	MessageListenerAdapter listenerAdapter(Receiver receiver) {
 		return new MessageListenerAdapter(receiver, "receiveMessage");
 	}
-
-	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange("spring-boot-exchange");
-	}
-
-	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with(queueName);
-	}
-
 
 	@Bean
 	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
@@ -49,8 +29,6 @@ public class DemoConsumerApplication {
 		container.setMessageListener(listenerAdapter);
 		return container;
 	}
-
-
 
 
 	public static void main(String[] args) {
