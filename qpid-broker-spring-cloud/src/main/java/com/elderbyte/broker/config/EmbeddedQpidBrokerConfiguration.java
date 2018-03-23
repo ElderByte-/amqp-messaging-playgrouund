abf75c210c0267449d9c90b4be12e68e184dedda
IsNull <p.buettiker@elderbyte.com>
2018-03-23 15:23:15 +0100

    more improvments

--- qpid-broker-spring-cloud/src/main/java/com/elderbyte/broker/config/EmbeddedQpidBrokerConfiguration.java
+++ qpid-broker-spring-cloud/src/main/java/com/elderbyte/broker/config/EmbeddedQpidBrokerConfiguration.java
@@ -1,6 +1,6 @@
 package com.elderbyte.broker.config;
 
-import org.apache.qpid.server.SystemLauncher;
+import com.elderbyte.broker.embedded.EmbededQpidBroker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Configures and starts an Qpid Message Broker.
@@ -18,65 +16,66 @@ import java.util.Map;
 @Configuration
 public class EmbeddedQpidBrokerConfiguration {
 
+    /***************************************************************************
+     *                                                                         *
+     * Fields                                                                  *
+     *                                                                         *
+     **************************************************************************/
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final String INITIAL_CONFIGURATION = "initial-config.json";
-    SystemLauncher brokerLauncher;
 
-    @Value("${server.qpid.port:5672}")
-    private String amqpPort;
+    @Value("${qpid.embedded.port:5672}")
+    private int amqpPort;
 
-    @Value(("${server.qpid.virtual-host:default}"))
+    @Value("${qpid.embedded.management.port:8085}")
+    private int httpPort;
+
+    @Value(("${qpid.embedded.vhost:default}"))
     private String virtualHost;
 
+    @Value(("${qpid.embedded.configurationUrl:}"))
+    private String configurationUrl;
 
-    public EmbeddedQpidBrokerConfiguration() {
-    }
+    private EmbededQpidBroker broker;
 
+    /***************************************************************************
+     *                                                                         *
+     * Constructor                                                             *
+     *                                                                         *
+     **************************************************************************/
+
+    public EmbeddedQpidBrokerConfiguration() {  }
+
+    /***************************************************************************
+     *                                                                         *
+     * Event hooks                                                             *
+     *                                                                         *
+     **************************************************************************/
 
     @PostConstruct
     public void startBroker(){
 
-        System.out.println("Starting QPID Broker @ " + virtualHost + " - port " + amqpPort + "... ");
-
-        try{
-            start();
-        }catch (Exception e){
-            throw new RuntimeException("Failed to start embedded QPID broker!", e);
+        if(configurationUrl == null || configurationUrl.isEmpty()){
+            URL initialConfig = EmbeddedQpidBrokerConfiguration.class.getClassLoader().getResource(INITIAL_CONFIGURATION);
+            configurationUrl = initialConfig.toString();
         }
+
+        broker = new EmbededQpidBroker(
+                amqpPort,
+                virtualHost,
+                httpPort,
+                configurationUrl
+        );
+
+        broker.start();
     }
 
     @PreDestroy
     public void shutdown(){
-        log.info("Shutting down broker...");
-        brokerLauncher.shutdown();
+        broker.shutdown();
     }
 
-    private void start() throws Exception {
-        brokerLauncher = new SystemLauncher();
-        brokerLauncher.startup(createSystemConfig());
-    }
 
-    private Map<String, Object> createSystemConfig(){
-        Map<String, Object> attributes = new HashMap<>();
-        URL initialConfig = EmbeddedQpidBrokerConfiguration.class.getClassLoader().getResource(INITIAL_CONFIGURATION);
-        attributes.put("type", "Memory");
-        attributes.put("initialConfigurationLocation", initialConfig.toExternalForm());
-        attributes.put("startupLoggedToSystemOut", true);
-        attributes.put("qpid.amqp_port", amqpPort);
-        attributes.put("qpid.http_port", 8085);
-        attributes.put("qpid.vhost", virtualHost);
-
-        /* Old config
-        brokerOptions.setConfigProperty("qpid.amqp_port", amqpPort);
-        brokerOptions.setConfigProperty("qpid.http_port", "8085");
-        brokerOptions.setConfigProperty("qpid.broker.defaultPreferenceStoreAttributes", "{\"type\": \"Noop\"}");
-        brokerOptions.setConfigProperty("qpid.vhost", virtualHost);
-        brokerOptions.setConfigurationStoreType("Memory");
-        brokerOptions.setStartupLoggedToSystemOut(false);
-         */
-
-
-        return attributes;
-    }
 }
