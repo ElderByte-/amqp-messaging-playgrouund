fe455aa2e8100412e2777937a55a125458c0dddd
IsNull <p.buettiker@elderbyte.com>
2018-03-23 16:04:45 +0100

    migrated embedded Qpid to custom spring-boot-starter project

--- producer/src/main/java/com/example/demo/DemoProducerAndBrokerApplication.java
+++ producer/src/main/java/com/example/demo/DemoProducerAndBrokerApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import com.elderbyte.broker.EnableEmbeddedBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@EnableEmbeddedBroker
 @SpringBootApplication
 public class DemoProducerAndBrokerApplication {
 
--- qpid-broker-spring-cloud/README.md
+++
@@ -1,7 +0,0 @@
-# qpid-broker-spring-cloud
-
-Provides an easy way to add a AMQP MQ broker to your spring based server.
-
-
-
-
