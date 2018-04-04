b982b6f0bf92c7a53e0e8afdab75c9afd59bc76a
IsNull <p.buettiker@elderbyte.com>
2018-04-04 14:56:18 +0200

    migrated to kafka-starter library

--- kafka-consumer/src/main/java/com/elderbyte/example/demo/config/KafkaConsumerConfiguration.java
+++ kafka-consumer/src/main/java/com/elderbyte/example/demo/config/KafkaConsumerConfiguration.java
@@ -1,10 +1,17 @@
 package com.elderbyte.example.demo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @Deprecated
 public class KafkaConsumerConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
 
 }
