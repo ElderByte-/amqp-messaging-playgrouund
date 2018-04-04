0e3b4058719fb45e28b9f4912979bc1cddf3ab0e
IsNull <p.buettiker@elderbyte.com>
2018-04-04 14:18:44 +0200

    clean up

--- kafka-producer/src/main/java/com/elderbyte/example/demo/SampleKafkaProducer.java
+++ kafka-producer/src/main/java/com/elderbyte/example/demo/SampleKafkaProducer.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
-
 @Component
 public class SampleKafkaProducer {
 
