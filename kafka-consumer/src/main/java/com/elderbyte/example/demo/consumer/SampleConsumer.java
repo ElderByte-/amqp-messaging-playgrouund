4eef70ab0b309edef231d1875596e190696312ea
IsNull <p.buettiker@elderbyte.com>
2018-04-05 13:40:58 +0200

    upgraded to java 10

--- kafka-consumer/src/main/java/com/elderbyte/example/demo/consumer/SampleConsumer.java
+++ kafka-consumer/src/main/java/com/elderbyte/example/demo/consumer/SampleConsumer.java
@@ -8,18 +8,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class SampleConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(SampleConsumer.class);
 
+    /*
     @KafkaListener(groupId= "worker-group", topics = "foobar")
     public void onMessage(ConsumerRecord<String, Json> record){
         onMessage(
                 record.key(),
                 record.value().json(SampleMessageDto.class).orElse(null)
         );
+    }*/
+
+    @KafkaListener(groupId= "worker-group-batch", topics = "foobar")
+    public void onMessage(List<ConsumerRecord<String, Json>> batch){
+        log.info("Received batch: " + batch.size());
     }
+
 
     private void onMessage(String key, SampleMessageDto message){
         log.info("KAFKA k:: " + key + ", MESSAGE:: " + message);
