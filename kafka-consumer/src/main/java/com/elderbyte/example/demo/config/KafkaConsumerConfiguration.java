064f9217a17f40725726d49ee99c20c653e4259e
IsNull <p.buettiker@elderbyte.com>
2018-04-06 09:07:24 +0200

    improved kafka sample

--- kafka-consumer/src/main/java/com/elderbyte/example/demo/config/KafkaConsumerConfiguration.java
+++ kafka-consumer/src/main/java/com/elderbyte/example/demo/config/KafkaConsumerConfiguration.java
@@ -1,12 +1,13 @@
 package com.elderbyte.example.demo.config;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+
 @Configuration
-@Deprecated
 public class KafkaConsumerConfiguration {
 
     @Bean
--- kafka-consumer/src/main/java/com/elderbyte/example/demo/consumer/SampleConsumer.java
+++ kafka-consumer/src/main/java/com/elderbyte/example/demo/consumer/SampleConsumer.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,22 +16,30 @@ public class SampleConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(SampleConsumer.class);
 
-    /*
-    @KafkaListener(groupId= "worker-group", topics = "foobar")
-    public void onMessage(ConsumerRecord<String, Json> record){
-        onMessage(
-                record.key(),
-                record.value().json(SampleMessageDto.class).orElse(null)
-        );
-    }*/
 
-    @KafkaListener(groupId= "worker-group-batch", topics = "foobar")
+
+    @KafkaListener(groupId= "worker-group-batch", topics = "foobar", containerFactory = "kafkaBatchFactory")
     public void onMessage(List<ConsumerRecord<String, Json>> batch){
         log.info("Received batch: " + batch.size());
+        batch.forEach(this::onReccord);
     }
 
 
-    private void onMessage(String key, SampleMessageDto message){
-        log.info("KAFKA k:: " + key + ", MESSAGE:: " + message);
+    @KafkaListener(groupId= "worker-group", topics = "foobar")
+    public void onMessage(ConsumerRecord<String, Json> record){
+        onReccord(record);
+    }
+
+
+    private void onReccord(ConsumerRecord<String, Json> record){
+
+        String key = record.key();
+        var dto = record.value().json(SampleMessageDto.class).orElse(null);
+
+        onReccord(key, dto);
+    }
+
+    private void onReccord(String key, SampleMessageDto message){
+        log.info("RECORD k:: " + key + ", MESSAGE:: " + message);
     }
 }
